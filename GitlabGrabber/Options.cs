a1b688f1378fbbac4ae9abd9438d4018e40c523b
Станислав Печезеров <pechezerov@megapolis-it.ru>
2022-12-08 10:53:48 +0300

    grabbing subgroups

---
+++ GitlabGrabber/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+public class Options
+{
+    [Option('o', "output", Required = true, HelpText = "Path to directory to save data")]
+    public string OutputPath { get; set; } = Environment.CurrentDirectory;
+
+    [Option('h', "host", Required = false, HelpText = "Url of gitlab server (default is https://gitlab.com)")]
+    public string GitlabHost { get; set; } = "https://gitlab.com";
+
+    [Option('t', "token", Required = true, HelpText = "Gitlab token")]
+    public string Token { get; set; } = default!;
+}
--- GitlabGrabber/Program.cs
+++ GitlabGrabber/Program.cs
@@ -1,39 +1,67 @@
 ﻿using CliWrap;
+using CommandLine;
 using Newtonsoft.Json;
 using System.Text;
 
-if (args.Count() < 3)
-{
-    Console.WriteLine("Usage:GitlabGrabber.exe targetlocation host token");
-    Console.WriteLine("Example: C:\\data\\gitlab https:\\gitlab.com glpat-zzzzxzxzxxxxzxzx");
-    return;
-}
-
-string targetPath = args[0].Replace("\"", "");
-string host = args[1];
-string token = args[2];
-
-// 1. get all groups
-var groupsResultSb = new StringBuilder();
-var cmdGroups = await Cli.Wrap("curl")
-    .WithArguments($"{host}/api/v4/groups -H \"PRIVATE-TOKEN: {token}\"")
-    .WithValidation(CommandResultValidation.None)
-    .WithStandardOutputPipe(PipeTarget.ToStringBuilder(groupsResultSb))
-    .ExecuteAsync();
-var groupsStringData = groupsResultSb.ToString();
-
 try
 {
-    var groups = JsonConvert.DeserializeObject<IEnumerable<Group>>(groupsStringData);
+    var options = Parser.Default.ParseArguments<Options>(args).Value;
 
-    // 2. get all projects
-    foreach (var group in groups)
+    // 1. get all groups
+    Queue<Group> groups;
+    try
     {
+        var groupsResultSb = new StringBuilder();
+        var cmdGroups = await Cli.Wrap("curl")
+            .WithArguments($"{options.GitlabHost}/api/v4/groups -H \"PRIVATE-TOKEN: {options.Token}\"")
+            .WithValidation(CommandResultValidation.None)
+            .WithStandardOutputPipe(PipeTarget.ToStringBuilder(groupsResultSb))
+            .ExecuteAsync();
+        var groupsStringData = groupsResultSb.ToString();
+        groups = new Queue<Group>(JsonConvert.DeserializeObject<IEnumerable<Group>>(groupsStringData) ?? Enumerable.Empty<Group>());
+
+        if (!groups.Any())
+            throw new InvalidOperationException("No groups");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}, skipping...");
+        groups = new Queue<Group>(Enumerable.Empty<Group>());
+    }
+
+    while (groups.Any())
+    {
+        // 2. recursively get all subgroups
+        var group = groups.Dequeue();
+        IEnumerable<Group> subgroups;
+        try
+        {
+            Console.WriteLine($"Collecting {group.name} subgroups...");
+
+            var subGroupsResultSb = new StringBuilder();
+            var cmdSubGroups = await Cli.Wrap("curl")
+                .WithArguments($"{options.GitlabHost}/api/v4/groups/{group.id}/subgroups -H \"PRIVATE-TOKEN: {options.Token}\"")
+                .WithValidation(CommandResultValidation.None)
+                .WithStandardOutputPipe(PipeTarget.ToStringBuilder(subGroupsResultSb))
+                .ExecuteAsync();
+            var subGroupsStringData = subGroupsResultSb.ToString();
+            subgroups = JsonConvert.DeserializeObject<IEnumerable<Group>>(subGroupsStringData) ?? Enumerable.Empty<Group>();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            subgroups = Enumerable.Empty<Group>();
+        }
+
+        foreach (var subgroup in subgroups)
+            groups.Enqueue(subgroup);
+
+        // 3. get all projects
         Console.WriteLine($"Grabbing {group.name}...");
 
         var groupResultSb = new StringBuilder();
         var cmdGroup = await Cli.Wrap("curl")
-            .WithArguments($"{host}/api/v4/groups/{group.id} -H \"PRIVATE-TOKEN: {token}\"")
+            .WithArguments($"{options.GitlabHost}/api/v4/groups/{group.id} -H \"PRIVATE-TOKEN: {options.Token}\"")
             .WithValidation(CommandResultValidation.None)
             .WithStandardOutputPipe(PipeTarget.ToStringBuilder(groupResultSb))
             .ExecuteAsync();
@@ -41,24 +69,24 @@ try
         var groupExtended = JsonConvert.DeserializeObject<GroupExtended>(groupResultSb.ToString());
         if (groupExtended.projects is null)
         {
-            Console.WriteLine($"No projects");
+            Console.WriteLine($"No projects in group {group.name}");
         }
         else
         {
-            // 3. grab all projects
+            // 4. grab all projects
             foreach (var project in groupExtended.projects)
             {
                 try
                 {
-                    var dirs = project.name_with_namespace.Split('/', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-                    var path = Path.Combine(new[] { targetPath }.Union(dirs).ToArray());
+                    var dirs = group.full_name.Split('/', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                    var path = Path.Combine(new[] { options.OutputPath }.Union(dirs).ToArray());
                     if (!Directory.Exists(path))
                         Directory.CreateDirectory(path);
 
                     Console.WriteLine($"Grabbing {project.name} to {path}");
 
                     var cmdProjectArchive = await Cli.Wrap("curl")
-                        .WithArguments($"{host}/api/v4/projects/{project.id}/repository/archive.zip -H \"PRIVATE-TOKEN: {token}\" --output \"{path}\\{project.name}.zip\"")
+                        .WithArguments($"{options.GitlabHost}/api/v4/projects/{project.id}/repository/archive.zip -H \"PRIVATE-TOKEN: {options.Token}\" --output \"{path}\\{project.name}.zip\"")
                         .WithValidation(CommandResultValidation.None)
                         .ExecuteAsync();
 
@@ -75,9 +103,8 @@ try
 
     Console.WriteLine($"OK =)");
 }
-catch
+catch (Exception ex)
 {
-    Console.WriteLine($"Error: {groupsStringData}, skipping...");
+    Console.WriteLine(ex.Message);
 }
-
 Console.ReadLine();
